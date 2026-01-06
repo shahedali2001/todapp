544b41b735dd313ae2ef4e5dd9bf0384622d9b3d
HASEEBA <haseebafaiza23@gmail.com>
2026-01-06 20:29:17 +0530

    whitecode

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,10 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: 'rgba(0, 245, 65, 1)', // Change this to your primary color
+      main: '#ebe6e4ff', // Change this to your primary color
     },
     secondary: {
-      main: '#10c775ff', // Change this to your secondary color
+      main: '#c4d3ccff', // Change this to your secondary color
     },
   },
 });
